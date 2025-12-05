3bfaef6e900f433250ab757ff93f5361a3b8e8f1
Sunly <sonlychao169@gmail.com>
2025-12-05 12:59:10 +0700

    update_Resume

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -55,7 +55,7 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1.2 }}
-          href="/simple-resume.pdf"
+          href="/Chao-Sunly-Resume.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
         >
